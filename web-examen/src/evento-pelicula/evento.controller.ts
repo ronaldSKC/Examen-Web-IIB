00429c64a4a898dc24118a0ffec1f750e3b04d40
Ronald <ronald_96c@hotmil.com>
2019-02-07 07:11:45 -0500

    ver-pelicula

--- web-examen/src/evento-pelicula/evento.controller.ts
+++ web-examen/src/evento-pelicula/evento.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Body, Delete, Req } from "@nestjs/common";
+import { Controller, Get, Param, Post, Body, Delete, Req, Res } from "@nestjs/common";
 import { EventoPeliculaService } from "./evento.service";
 import { EventoPeliculaCreateDto } from "./evento-pelicula-create-dto/evento-pelicula-create.dto";
 import { EventoPeliculaUpdateDto } from "./evento-pelicula-update-dto/evento-pelicula-update.dto";
@@ -22,6 +22,19 @@ export class EventoPeliculaController {
         @Param('id') id
     ) {
         return this._eventoPeliculaService.findOne(id);
+    }
+    @Get('evento-pelicula')
+    async verEvento(
+        @Res() res
+    ){
+        const eventoPeliculas = this._eventoPeliculaService.findAll()
+        let evel
+       await eventoPeliculas.then((evnt)=>{
+            evel = evnt.filter(peliculas => peliculas.evento.id === 1 )
+        })
+        res.render('evento-pelicula',{
+            arreglo: evel
+        })
     }
     @Post('crear')
     create(
